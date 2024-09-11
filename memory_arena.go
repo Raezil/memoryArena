4e0f772271da9766e3d20643b6afae198cbe9535
Raezil <kamil.mazurek319@gmail.com>
2024-09-11 19:20:43 +0200

    feat: memoryArena buffer (#9)

--- memory_arena.go
+++ memory_arena.go
@@ -9,10 +9,23 @@ import (
 // memory: A byte array that holds the actual memory
 // size the total size of the memory arena
 // offset the amount of memory currently in use
-type MemoryArena[T any] struct {
+type MemoryArenaBuffer struct {
 	memory []byte
 	size   int
 	offset int
+}
+
+// this function creates a new memory arena buffer of a specified size
+func NewMemoryArenaBuffer(size int) *MemoryArenaBuffer {
+	return &MemoryArenaBuffer{
+		memory: make([]byte, size),
+		size:   size,
+		offset: 0,
+	}
+}
+
+type MemoryArena[T any] struct {
+	buffer MemoryArenaBuffer
 }
 
 // this function creates a new memory arena of a specified size
@@ -22,28 +35,26 @@ func NewMemoryArena[T any](size int) (*MemoryArena[T], error) {
 		return nil, fmt.Errorf("cannot initialize, size below 0")
 	}
 	arena := MemoryArena[T]{
-		memory: make([]byte, size),
-		size:   size,
-		offset: 0,
+		buffer: *NewMemoryArenaBuffer(size),
 	}
 	return &arena, nil
 }
 
 // this function aligns the offset to the specified alignment
 func (arena *MemoryArena[T]) alignOffset(alignment uintptr) {
-	if (arena.offset % int(alignment)) != 0 {
-		arena.offset = (arena.offset + int(alignment-1)) &^ (int(alignment) - 1)
+	if (arena.buffer.offset % int(alignment)) != 0 {
+		arena.buffer.offset = (arena.buffer.offset + int(alignment-1)) &^ (int(alignment) - 1)
 	}
 }
 
 // Remaining capacity of the arena
 func (arena *MemoryArena[T]) UsedCapacity(size int) int {
-	return arena.offset + size
+	return arena.buffer.offset + size
 }
 
 // checking boundries of the arena
 func (arena *MemoryArena[T]) ArenasBoundary(size int) bool {
-	return arena.UsedCapacity(size) > arena.size
+	return arena.UsedCapacity(size) > arena.buffer.size
 }
 
 // this function is used to allocate memory from the arena
@@ -62,21 +73,21 @@ func (arena *MemoryArena[T]) Allocate(size int) (unsafe.Pointer, error) {
 		return nil, fmt.Errorf("not enough space left in the arena")
 	}
 
-	result := unsafe.Pointer(&arena.memory[arena.offset])
-	arena.offset += size
+	result := unsafe.Pointer(&arena.buffer.memory[arena.buffer.offset])
+	arena.buffer.offset += size
 	return result, nil
 }
 
 // this function frees the memory in the arena by setting all the bytes to 0
 func (arena *MemoryArena[T]) Free() {
-	for i := range arena.memory {
-		arena.memory[i] = 0
+	for i := range arena.buffer.memory {
+		arena.buffer.memory[i] = 0
 	}
 }
 
 // this function resets the arena by setting the offset to 0
 func (arena *MemoryArena[T]) Reset() {
-	arena.offset = 0
+	arena.buffer.offset = 0
 	arena.Free()
 }
 
