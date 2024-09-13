b48eab5682fc738fe78a01ca9697f3b90f3ef6c1
Raezil <kamil.mazurek319@gmail.com>
2024-09-13 11:40:32 +0200

    test: new tests

--- arenas_test.go
+++ arenas_test.go
@@ -58,6 +58,21 @@ func TestAppendSlice(t *testing.T) {
 	}
 }
 
+func BenchmarkAppendSlice(b *testing.B) {
+	arena, err := NewMemoryArena[[]int](100)
+	if err != nil {
+		b.Errorf("Error: %v", err)
+	}
+	obj := 5
+	slice := []int{1, 2, 3}
+	for i := 0; i < b.N; i++ {
+		_, err = AppendSlice(&obj, arena, &slice)
+		if err != nil {
+			b.Errorf("Error: %v", err)
+		}
+	}
+}
+
 func TestSetNewValue(t *testing.T) {
 	arena, err := NewMemoryArena[int](100)
 	if err != nil {
@@ -71,6 +86,24 @@ func TestSetNewValue(t *testing.T) {
 	ptr, _ = SetNewValue(&ptr, obj)
 	if ptr == nil {
 		t.Errorf("Error: ptr is nil")
+	}
+}
+
+func BenchmarkSetNewValue(b *testing.B) {
+	arena, err := NewMemoryArena[int](100)
+	if err != nil {
+		b.Errorf("Error: %v", err)
+	}
+	obj := 5
+	ptr, err := arena.Allocate(10)
+	if err != nil {
+		b.Errorf("Error: %v", err)
+	}
+	for i := 0; i < b.N; i++ {
+		ptr, _ = SetNewValue(&ptr, obj)
+		if ptr == nil {
+			b.Errorf("Error: ptr is nil")
+		}
 	}
 }
 
@@ -96,6 +129,20 @@ func TestConcurrentArena_AllocateObject(t *testing.T) {
 	_, err = concurrentArena.AllocateObject(obj)
 	if err != nil {
 		t.Errorf("Error: %v", err)
+	}
+}
+
+func BenchmarkNewObject(b *testing.B) {
+	arena, err := NewMemoryArena[int](100)
+	if err != nil {
+		b.Errorf("Error: %v", err)
+	}
+	obj := 5
+	for i := 0; i < b.N; i++ {
+		_, err = NewObject(arena, obj)
+		if err != nil {
+			b.Errorf("Error: %v", err)
+		}
 	}
 }
 
